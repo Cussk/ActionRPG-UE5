a3419face520d96a66ded6707c12aff75d07e509
Kyle Cuss <108804713+Cussk@users.noreply.github.com>
2024-03-04 08:18:38 -0330

    Setup Item base class

---
+++ Source/Soulslike/Private/Items/Item.cpp
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Items/Item.h"
+
+AItem::AItem()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	StaticMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>("StaticMeshComponent");
+	RootComponent = StaticMeshComponent;
+}
+
+void AItem::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AItem::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	RunningTime += DeltaTime;
+}
+
+float AItem::TransformedSin()
+{
+	return Amplitude * FMath::Sin(RunningTime * TimeConstant);
+}
+
+float AItem::TransformedCos()
+{
+	return Amplitude * FMath::Cos(RunningTime * TimeConstant);
+}
+
